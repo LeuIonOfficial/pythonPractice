1740bd1d1570739c2232246cc226433eb477fab6
Ion Leu <leuionofficial@gmail.com>
2023-02-21 20:17:06 +0200

    Program to find the Nth term in Fibonacci series using recursion

--- pr_1.py
+++ pr_1.py
@@ -1,5 +1,12 @@
-my_list = [2, 5, 6, 7, 8, 9, 3, 42, 1, 5]
+x = int(input('Nth term: '))
 
-del my_list[1]
+num1, num2 = 0, 1
 
-print(my_list)
+list_of_fibonacci_numbers = [num1, num2]
+
+for i in range(x - 1):
+    result = num1 + num2
+    num1, num2 = num2, result
+    list_of_fibonacci_numbers.append(result)
+
+print(list_of_fibonacci_numbers[-1])
