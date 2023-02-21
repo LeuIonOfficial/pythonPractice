52b58e82371494337176454700880fe696d51c99
Ion Leu <leuionofficial@gmail.com>
2023-02-21 22:59:25 +0200

    find odd numbers in array list

--- pr_1.py
+++ pr_1.py
@@ -1,13 +1,6 @@
-matrix1 = [[1, 2], [3, 4]]
-matrix2 = [[5, 6], [7, 8]]
 
-result_matrix = []
+my_array = [1, 1, 1, 4, 5]
 
-for i in range(len(matrix1)):
-    row = []
-    for el in range(len(matrix1[i])):
-        result = matrix1[i][el] * matrix2[i][el]
-        row.append(result)
-    result_matrix.append(row)
-
-print(result_matrix)
+for el in my_array:
+    if el % 2 == 0:
+        print(el)
