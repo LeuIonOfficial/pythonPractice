07d371bf1ed75500b2eb0d2fa7a8e27bf4b70184
Ion Leu <leuionofficial@gmail.com>
2023-02-21 09:02:22 +0200

    palindrom check

--- pr_1.py
+++ pr_1.py
@@ -1,44 +1,9 @@
-def add(x, y):
-    return x + y
-
-
-def substract(x, y):
-    return x - y
-
-
-def multiply(x, y):
-    return x * y
-
-
-def divide(x, y):
-    return x / y
-
-
-while True:
-
-    print("""
-  1: for add,
-  2: for substract,
-  3: for multiply,
-  4: for divide  
-  """)
-
-    option = int(input("Option: "))
-
-    num1 = float(input("Num 1: "))
-    num2 = float(input("Num 2: "))
-
-    if option == 1:
-        result = add(num1, num2)
-        print(f"{num1} + {num2} = {result:.2f}")
-    elif option == 2:
-        result = substract(num1, num2)
-        print(f"{num1} - {num2} = {result:.2f}")
-    elif option == 3:
-        result = multiply(num1, num2)
-        print(f"{num1} * {num2} = {result:.2f}")
-    elif option == 4:
-        result = divide(num1, num2)
-        print(f"{num1} / {num2} = {result:.2f}")
+def palindrome_check():
+    word = input('word: ')
+    if word[0] == word[-1]:
+        print("Palindrome")
     else:
-        continue
+        print("Not palindrome")
+
+
+palindrome_check()
