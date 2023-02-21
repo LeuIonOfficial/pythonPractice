ebd5a4c6c0d6ea0f567741d48814a89d4c1bce4a
Ion Leu <leuionofficial@gmail.com>
2023-02-21 08:56:38 +0200

    a calculator to do basic operations

--- pr_1.py
+++ pr_1.py
@@ -1,12 +1,44 @@
-import math
+def add(x, y):
+    return x + y
 
 
-def get_circle_info_given_radius(radius):
-    circumference = 2 * math.pi * radius
-    area = math.pi * radius * radius
-    return f"{float(circumference):.2f}, {float(area):.2f}"
+def substract(x, y):
+    return x - y
 
 
-radius = int(input('Radius: '))
+def multiply(x, y):
+    return x * y
 
-print(get_circle_info_given_radius(radius))
+
+def divide(x, y):
+    return x / y
+
+
+while True:
+
+    print("""
+  1: for add,
+  2: for substract,
+  3: for multiply,
+  4: for divide  
+  """)
+
+    option = int(input("Option: "))
+
+    num1 = float(input("Num 1: "))
+    num2 = float(input("Num 2: "))
+
+    if option == 1:
+        result = add(num1, num2)
+        print(f"{num1} + {num2} = {result:.2f}")
+    elif option == 2:
+        result = substract(num1, num2)
+        print(f"{num1} - {num2} = {result:.2f}")
+    elif option == 3:
+        result = multiply(num1, num2)
+        print(f"{num1} * {num2} = {result:.2f}")
+    elif option == 4:
+        result = divide(num1, num2)
+        print(f"{num1} / {num2} = {result:.2f}")
+    else:
+        continue
