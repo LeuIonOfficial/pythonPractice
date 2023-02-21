0cb94402ca4e0b8fd9a0eff097e6daab8e7bd9df
Ion Leu <leuionofficial@gmail.com>
2023-02-21 22:46:16 +0200

    implement matrix addition

--- pr_1.py
+++ pr_1.py
@@ -1,16 +1,13 @@
-def leap_year(year):
-    if year % 4 == 0:
-        if year % 100 == 0 and year % 400 != 0:
-            return False
-        else:
-            return True
-    else:
-        return False
+matrix1 = [[1, 2], [3, 4]]
+matrix2 = [[5, 6], [7, 8]]
 
+result_matrix = []
 
-year = 2024
+for i in range(len(matrix1)):
+    row = []
+    for el in range(len(matrix1[i])):
+        result = matrix1[i][el] + matrix2[i][el]
+        row.append(result)
+    result_matrix.append(row)
 
-if leap_year(year):
-    print(year, 'is a leap year')
-else:
-    print(year, 'isnt a leap year')
+print(result_matrix)
