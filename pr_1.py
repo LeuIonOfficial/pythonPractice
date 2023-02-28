187856e2193e7bb34b699b7f62fd4e739a0e5eb2
Ion Leu <leuionofficial@gmail.com>
2023-02-28 14:51:43 +0200

    python program to insert a number to any position in a list

--- pr_1.py
+++ pr_1.py
@@ -1,6 +1,10 @@
-my_list = input("numbers: ").split(',')
-result = 0
-for el in my_list:
-    result += int(el)
-print(result)
+def post_nr_in_list(my_list):
+    x = int(input('Index of place: '))
+    nr = int(input('NR you want to place: '))
+    my_list[x] = nr
+    return my_list
 
+
+if __name__ == '__main__':
+    new_list = [4, 2, 4, 5, 6, 7, 8, 91]
+    print(post_nr_in_list(new_list))
